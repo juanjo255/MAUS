ac1ce5b0769ec05503493d6d05ac3db5ed6c6f87
juanjo255 <jjpiconc@eafit.edu.co>
2024-05-29 15:41:17 +0000

    adding kraken2

--- MAUS_cli.sh
+++ MAUS_cli.sh
@@ -1,9 +1,10 @@
 ## options
 
-fastp_options=" "
 wd="./MAUS_result/"
 prefix1=""
 prefix2=""
+threads=4
+fastp_options=""
 
 MAUS_help() {
     echo "
@@ -15,19 +16,21 @@ MAUS_help() {
     Version: 0.1
 
     Usage: MAUS_cli.sh [options] -1 reads_R1.fastq -2 reads_R2.fastq
-
+--thread
     Options:
         -1        Input R1 paired end file. [required].
         -2        Input R2 paired end file. [required].
+        -d        Database for kraken. if you do not have one, create one before using this pipeline. [required].
         -t        Threads. [4].
         -w        Working directory. Path to create the folder which will contain all MAUS information. [./MAUS_result].
-        -d        Different output directory. Create a different output directory every run (it uses the date and time). [False].
+        -z        Different output directory. Create a different output directory every run (it uses the date and time). [False].
         -f        FastP options. [\" \"].
+        
         *         Help.
     "
     exit 1
 }
-while getopts '1:2:t:w:d:f:' opt; do
+while getopts '1:2:d:t:w:z:f:' opt; do
     case $opt in
         1)
         input_R1_file=$OPTARG
@@ -35,13 +38,16 @@ while getopts '1:2:t:w:d:f:' opt; do
         2)
         input_R2_file=$OPTARG
         ;;
+        d)
+        kraken2_db=$OPTARG
+        ;;
         t)
         threads=$OPTARG
         ;;
         w)
         wd=$OPTARG
         ;;
-        d)
+        z)
         output_dir="MAUS_result_$(date  "+%Y-%m-%d_%H-%M-%S")/"
         ;;
         f)
@@ -60,6 +66,7 @@ if [ $OPTIND -eq 1 ]; then MAUS_help; fi
 ## Check required files are available
 if [ -z $input_R1_file ]; then echo "ERROR => File 1 is missing"; MAUS_help; fi
 if [ -z $input_R2_file ]; then echo "ERROR => File 2 is missing"; MAUS_help; fi
+if [ -z $kraken2_db ]; then echo "ERROR => Kraken2 database is missing"; MAUS_help; fi
 
 ## Check if working directory has the last slash
 if [ ${wd: -1} = / ];
@@ -96,8 +103,15 @@ fastp_preprocess (){
     echo "**** Quality filter with fastp *****"
     echo " "
     echo "FastP options: $fastp_options"
-    fastp -i $input_R1_file -I $input_R2_file $fastp_options -o $wd$prefix1".filt.fastq" -O $wd$prefix2".filt.fastq" -j $wd$prefix2".html" -h $wd$prefix2".json"
+    fastp --thread $threads -i $input_R1_file -I $input_R2_file $fastp_options -o $wd$prefix1".filt.fastq" -O $wd$prefix2".filt.fastq" -j $wd$prefix2".html" -h $wd$prefix2".json"
 }
+
+
+## Kraken2 classification
+Kraken2_class (){
+    kraken2 --threads $threads --db $kraken2_db $input_R1_file $input_R2_file 
+}
+
 
 
 ## PIPELINE
@@ -110,4 +124,4 @@ else
     create_wd
 fi
 
-fastp_preprocess
+#fastp_preprocess
