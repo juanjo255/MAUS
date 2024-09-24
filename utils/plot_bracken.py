ee613906d90209b43fa5a0abe91c79fb6e279735
juanjo255 <piconcossio@gmail.com>
2024-09-24 13:29:42 -0500

    plotting bracken

--- utils/plot_bracken.py
+++ utils/plot_bracken.py
@@ -1,8 +1,10 @@
 ## This is to plot Bracken results per sample along 
 ## with diversity metric (shannon so far)
 import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
-min_abundance=0.1
+min_abundance=1
 samples=6
 ## Open file
 bracken_file=pd.read_csv(filepath_or_buffer='/Volumes/PiconCossio/biocomp_tools/MAUS/utils/braken_output_G1_G2.txt', 
@@ -20,6 +22,22 @@ def filter_by_abundance_conditional (df, number_samples, min_abundance, min_samp
     df = df [(df.iloc[:,number_samples*-1:] >= min_abundance).sum(axis=1) > min_samples]
     return df
 
+## Plot
+def plot_stacked(df, sample_names, 
+                 title_plot="Relative abundance V3-V4 Kraken2-Bracken"):
+    ## It must be this way, remember some Genus have same name, like uncultured 
+    sample_names=df.columns
+    fig = make_subplots(rows=2, cols=1)
+    for i in range(0, len(df["name"])):
+        fig.add_trace(go.Bar(name=df.iloc[i,0].strip(),
+                            x=sample_names,y=df.iloc[i,1:]),
+                    row=1, col=1)
 
+    fig.update_layout(barmode='stack',
+        autosize=True,
+        width=1200,
+        height=800,
+        title_text=title_plot)
+    fig.show()
 
 
