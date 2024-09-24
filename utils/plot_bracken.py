30d7a30027cf84143c4f70a580161f457a702c4d
juanjo255 <piconcossio@gmail.com>
2024-09-23 23:53:21 -0500

    paring file produced by combine_kreports.py for plotting

--- utils/plot_bracken.py
+++ utils/plot_bracken.py
@@ -3,23 +3,23 @@
 import pandas as pd
 
 min_abundance=0.1
-
+samples=6
 ## Open file
 bracken_file=pd.read_csv(filepath_or_buffer='/Volumes/PiconCossio/biocomp_tools/MAUS/utils/braken_output_G1_G2.txt', 
-                         delimiter='\t', skiprows=4)
+                         delimiter='\t', skiprows=8)
 
-## compute relative abundance (RelAbun)
-def compute_relative_abun (number_samples=6, df):
+## Compute relative abundance (RelAbun)
+def compute_relative_abun (df, number_samples=6):
     for i in range(1, number_samples+1):
         df[f"G{i}_1_RelAbun"] = df[f"G{i}_1_lvl"]/df[f"G{i}_1_lvl"].sum() * 100
 
-def filter_by_abundance_conditional (df, min_abundance, min_samples):
+## Filter by abundance 
+def filter_by_abundance_conditional (df, number_samples, min_abundance, min_samples):
     ## This is intended to mimic the taxa filter by abundance conditionally of qiime2
     ## Select taxa that have at least n abundance in at least n samples 
     df = df [(df.iloc[:,number_samples*-1:] >= min_abundance).sum(axis=1) > min_samples]
     return df
-## Filter by abundance 
-bracken_file_gt_01 = bracken_file[(bracken_file["G1_1_RelAbun"]>=min_abundance) | (bracken_file["G2_1_RelAbun"]>=min_abundance)]
+
 
 
 
