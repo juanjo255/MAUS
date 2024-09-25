9605d4b2eb2897c94ff3b0a2041e10fa1c4d4a6a
juanjo255 <piconcossio@gmail.com>
2024-09-25 10:30:11 -0500

    error with conditional

--- MAUS_cli.sh
+++ MAUS_cli.sh
@@ -168,23 +168,23 @@ fastp_filter (){
         echo " "
         echo "**** Quality filter with fastp *****"
         echo " "
-        echo "FastP options: $fastp_options"
+        
         merge_mode=$(echo $fastp_options | grep -Ewo -- "-m|--merge")
         if ! [ -z "$merge_mode" ];then
+            fastp_options_backup=$fastp_options
             fastp_options="$fastp_options --merge_out $wd$prefix1.merge.fastq"
             ## Use the merged reads
             input_R1_file=$wd$prefix1".merge.fastq"
             input_R2_file=""
-
         else
             # Use the filtered reads in the rest of the pipeline
             input_R1_file=$wd$prefix1".filt.fastq"
             input_R2_file=$wd$prefix2".filt.fastq"
         fi
-
+        echo "FastP options: $fastp_options"
         fastp --thread $threads $fastp_options -i $input_R1_file -I $input_R2_file -o $wd$prefix1".filt.fastq" -O $wd$prefix2".filt.fastq" \
             -j $wd$prefix1".json" -h $wd$prefix1".html"
-        
+        fastp_options=$fastp_options_backup
        
     fi
 }
